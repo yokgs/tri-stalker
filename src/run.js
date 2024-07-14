9e461b6f3bf65ebee87dd522d473c97c5f34ec4c
yokgs <yazid.slila01@gmail.com>
2024-07-14 21:30:02 +0100

    fix : ignore \r in the input

--- src/run.js
+++ src/run.js
@@ -2,7 +2,7 @@ const fs = require("fs"),
     axios = require("axios"),
     path = require("path");
 const { URL, MODES } = require('./consts');
-
+const $ = { r: "\x1b[0m", g: "\x1b[32m", s: "\x1b[47m", k: "\x1b[30m", e: "\x1b[31m" }
 const mode$ = process.argv[2];
 let mode;
 
@@ -17,7 +17,7 @@ console.log(`Running Mode ${mode}...`);
 mode = ['t', 'f'][MODES.indexOf(mode)];
 
 async function save(name, code, id) {
-    console.log(`Downloading ${name} resume`);
+    console.log(`Downloading ${$.g}${name.toUpperCase()}${$.r} resume!`);
     let url = URL(code, name, id);
     try {
         let response = await axios.get(url, {
@@ -26,8 +26,11 @@ async function save(name, code, id) {
 
         if (response.data) {
             let file = path.parse(url).base;
-            fs.writeFileSync(file, Buffer.from(response.data, 'binary'));
-            console.log(`${name} resume : FINISHED`);
+            let destination = path.join(process.cwd(), mode$.toUpperCase());
+            if(!fs.existsSync(destination)) fs.mkdirSync(destination, { recursive : true });
+
+            fs.writeFileSync(path.join(destination, file), Buffer.from(response.data, 'binary'));
+            console.log(`${$.s}${$.g}${name.toUpperCase()}${$.k} resume : FINISHED${$.r}`);
             return true;
         }
     } catch (_) { return false; }
@@ -41,16 +44,17 @@ function toId(num) {
 }
 
 async function dwnAll() {
-    let data = fs.readFileSync("./data.txt").toString().split("\n");
+    let data = fs.readFileSync("./data.txt").toString().trim().split("\n").map(x => x.trim());
     let existed = [];
     for (let name of data) {
-        for (let i = 1; i < 300; i++) {
+        for (let i = 1; i < 300; i += i < 100 ? 1 : 10) {
             if (existed.includes(i)) continue;
             let done = await save(name, mode, toId(i));
             if (done) {
                 existed.push(i);
                 break;
             }
+            if(i == 290) console.log(`${$.s}${$.k}Oops! Cannot find ${$.e}${name.toUpperCase()}${$.r}${$.s}${$.k}'s resume!${$.r}`);
         }
     }
 }
